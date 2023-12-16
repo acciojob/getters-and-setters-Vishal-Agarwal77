487181aaf6e72558ef7fb476ff61e4c7d36bcbd4
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-12-16 17:22:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //complete this code
 class Person {
 	constructor(name,age){
-		this._name=name;
-		this._age=age;
+		this.name=name;
+		this.age=age;
 	}
-	get namevalue(){
-		console.log(this._name);
+	get name(){
+		console.log(this.name);
 	}
 	set agevalue(age){
-		this._age=age;
+		this.age=age;
 	}
 }
 
