ef49a03c0e6766ef603accf45484feee1ed80556
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-12-16 17:40:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 //complete this code
 class Person {
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
-	}
-	get name(){
-		console.log(this.name);
-	}
-	set agevalue(age){
-		this.age=age;
-	}
+	constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set age(age) {
+    this._age = age;
+  }
+
+  get age() {
+    return this._age;
+  }
 }
 
 class Student extends Person {
 	study(){
-		console.log(`${this.name} is studying`);
+		console.log(`${this._name} is studying`);
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${this.name} is teaching`);
+		console.log(`${this._name} is teaching`);
 	}
 }
 
